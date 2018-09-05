1067c0535ca072d7cd949f41c465a94cfdddbfec
Howie Tang <howie.tang@bitsinglass.com>
2018-09-05 11:26:10 -0400

    add exception handling

---
+++ exercise/sfaccounts/src/main/java/sfaccounts/exception/InvalidParameterException.java
@@ -0,0 +1,15 @@
+package sfaccounts.exception;
+
+public class InvalidParameterException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5045153413549327877L;
+
+	public InvalidParameterException(String message) {
+		super(message);
+	}
+
+	
+}
